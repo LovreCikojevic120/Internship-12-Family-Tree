5c4eadb3e7b981bc9d754a0a6c827e653602a093
LovreCikojevic120 <56341053+LovreCikojevic120@users.noreply.github.com>
2022-02-09 18:08:43 +0100

    Changed family tree structure, added some input functions

--- index.js
+++ index.js
@@ -1,52 +1,83 @@
-class Person {
-    constructor(
-        name,
-        surname,
-        gender,
-        birthYear,
-        deathYear,
-        father,
-        mother,
-        companion
-    ) {
-        this.name = name;
-        this.surname = surname;
-        this.gender = gender;
-        this.birthYear = birthYear;
-        this.deathYear = deathYear;
-        this.father = father;
-        this.mother = mother;
-        this.companion = companion;
-    }
-}
+const familyTree = [
+	root = {
+		name: 'sdvjnsdvk',
+		surname:'skdjfbskd',
+		gender: 'male',
+		parents: [],
+		children: [],
+		companion: rootCompanion,
+		birthYear: 1900,
+		deathYear: 1944
+	},
+
+	rootCompanion = {
+		name: 'rtuzrmlnw',
+		surname:'skdjfbskd',
+		oldSurname: 'gbjndlvdd',
+		gender: 'female',
+		parents: [],
+		children: [],
+		companion: root,
+		birthYear: 1902,
+		deathYear: 1956 
+	},
+
+	rootChild1 = {
+		name: 'rtuzrmlnw',
+		surname:'skdjfbskd',
+		gender: 'female',
+		parents: [root, rootCompanion],
+		children: [],
+		companion: null,
+		birthYear: 1922,
+		deathYear: 1978 
+	},
+
+	rootChild2 = {
+		name: 'rtuzrmlnw',
+		surname:'skdjfbskd',
+		gender: 'male',
+		parents: [root, rootCompanion],
+		children: [],
+		companion: null,
+		birthYear: 1924,
+		deathYear: 1990 
+	}
+]
+
+const mainMenuChoices = {
+    AddNewMember: 1,
+    AddFamilyDeath: 2,
+    ShowStatistics: 3,
+    Exit: 0,
+};
 
 let menuChoice;
 
 while (menuChoice !== 0) {
-	
     menuChoice = parseInt(
         prompt(
-            "Welcome, choose menu option:\n"+
-			"1 - Add a family member\n"+
-			"2 - Input family death\n"+
-			"3 - Statistics\n"+
-			"0 - Exit application"
+            "Welcome, choose menu option:\n" +
+                "1 - Add a family member\n" +
+                "2 - Input family death\n" +
+                "3 - Statistics\n" +
+                "0 - Exit application"
         )
     );
 
-	console.log(menuChoice);
+    console.log(menuChoice);
 
     switch (menuChoice) {
-        case 1:
+        case mainMenuChoices.AddNewMember:
             AddMember();
             break;
-        case 2:
+        case mainMenuChoices.AddFamilyDeath:
             InputDeath();
             break;
-        case 3:
+        case mainMenuChoices.ShowStatistics:
             Statistics();
             break;
-        case 0:
+        case mainMenuChoices.Exit:
             break;
         default:
             HandleError();
@@ -55,18 +86,61 @@ while (menuChoice !== 0) {
 }
 
 function AddMember() {
-    let inputMemberMenuChoice = prompt("");
-	alert('add');
+    let inputMemberMenuChoice;
+
+    while (inputMemberMenuChoice !== 0) {
+        inputMemberMenuChoice = parseInt(
+            prompt("1 - Child\n2 - Marriage companion\n0 - Back to main menu")
+        );
+
+        switch (inputMemberMenuChoice) {
+            case 1:
+                InputChild();
+                break;
+            case 2:
+                InputCompanion();
+                break;
+            case 0:
+                return;
+            default:
+                HandleError();
+                break;
+        }
+    }
 }
 
-function InputDeath(){
-	alert('ded');
+function InputCompanion() {
+    const person = {};
+    person.name = StringInput("name");
+    person.surname = StringInput("surname");
+	person.father = familyTree.findIndex(person => person);
+    familyTree.push(person);
 }
 
-function Statistics(){
-	alert('stst');
+function InputChild() {
+    alert("child");
 }
 
-function HandleError(){
-	alert('error');
+function InputDeath() {
+    alert("ded");
+}
+
+function Statistics() {
+    alert("stst");
+}
+
+function HandleError() {
+    alert("error");
+}
+
+function StringInput(inputType) {
+    let result;
+
+    while (result !== "") {
+        result = prompt(
+            `Insert ${inputType}, to cancel input enter empty string`
+        );
+
+        if (result !== " " && isNaN(result)) return result;
+    }
 }
